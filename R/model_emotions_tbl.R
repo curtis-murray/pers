4e3b438c26ffe96a93f96c433353739218da7d5d
curtis-murray <murrcurt09@gmail.com>
2022-12-13 00:59:43 +1030

    adding unique doc ID

--- R/model_emotions_tbl.R
+++ R/model_emotions_tbl.R
@@ -29,33 +29,34 @@ model_emotions_tbl <- function(tbl, input, batch_size = NULL, quiet = T, show_pr
   while((tbl_itr-1)*batch_size < tbl_len){
     if(!quiet) print(paste("Processing batch: ", tbl_itr, " of ", ceiling(tbl_len/batch_size)),sep = "")
     out <- bind_rows(out, tbl[((tbl_itr-1)*batch_size+1):min(tbl_len,tbl_itr*batch_size),] %>%
+                       mutate(DOC_ID_model_emotions_tbl = 1:n()) %>%
                        mutate(Report_tmp = get(input)) %>%
-                       group_by(Report_tmp) %>%
+                       group_by(DOC_ID_model_emotions_tbl,Report_tmp) %>%
                        mutate(group_id = ifelse((1:n())==1,1,0)) %>%
                        ungroup() %>%
                        mutate(group_id = cumsum(group_id)*group_id) %>%
                        unnest_tokens(word,Report_tmp,drop = FALSE) %>%
                        left_join(model, by = "word") %>%
                        drop_na() %>%
-                       group_by(Report_tmp, Emotion,prior, group_id) %>%
+                       group_by(DOC_ID_model_emotions_tbl,Report_tmp, Emotion,prior, group_id) %>%
                        summarise(ls = sum(log(likelihood)),
                                  likelihood = prod(likelihood),.groups = "keep") %>%
                        mutate(ak = ls+log(prior)) %>%
-                       group_by(Report_tmp) %>%
+                       group_by(DOC_ID_model_emotions_tbl,Report_tmp) %>%
                        mutate(A = max(ak),
                               denom = A + log(sum(exp(ak-A))),
                               ll = ls+log(prior)-denom,
                               post = exp(ls+log(prior)-denom)) %>%
-                       group_by(Report_tmp) %>%
+                       group_by(DOC_ID_model_emotions_tbl,Report_tmp) %>%
                        select(Report_tmp, Emotion, Prior = prior, Posterior = post, group_id) %>%
                        left_join(emotion_clustering %>% select(-count), by = "Emotion") %>%
-                       group_by(Report_tmp, Cluster) %>%
+                       group_by(DOC_ID_model_emotions_tbl,Report_tmp, Cluster) %>%
                        mutate(Cluster_Posterior = sum(Posterior)) %>%
                        mutate(Posterior = ifelse(is.na(Posterior), Prior, Posterior)) %>%
-                       group_by(Report_tmp) %>%
+                       group_by(DOC_ID_model_emotions_tbl,Report_tmp) %>%
                        arrange(group_id, -Posterior) %>%
                        ungroup() %>%
-                       select(-group_id)) %>%
+                       select(-group_id,-DOC_ID_model_emotions_tbl)) %>%
       ungroup()
     tbl_itr <- tbl_itr + 1
   }
@@ -65,3 +66,6 @@ model_emotions_tbl <- function(tbl, input, batch_size = NULL, quiet = T, show_pr
   left_join(tbl, out, by = input)
 
 }
+
+# Test to add:
+#model_emotions_tbl(tibble(word = rep("public",10)), "word") %>% filter(Emotion == "in pain") should all have same posterior
